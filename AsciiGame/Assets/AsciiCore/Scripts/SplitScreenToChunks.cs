e4156a6d2b39762606df21c347883fe4a88f6dd9
Marcin Czekaj <mar.czekaj11@gmail.com>
2023-02-04 06:14:32 +0100

    fix(Ascii system): fixed ascii system calculations

--- AsciiGame/Assets/AsciiCore/Scripts/SplitScreenToChunks.cs
+++ AsciiGame/Assets/AsciiCore/Scripts/SplitScreenToChunks.cs
@@ -6,29 +6,44 @@ namespace Krakjam
     public sealed class SplitScreenToChunks : MonoBehaviour
     {
         #region Inspector Variables
+        [Header("Shaders")]
+        [SerializeField] private ComputeShader _PrepareTextureToGenerateAsciiTexture;
+
+        [Header("Materials")]
+        [SerializeField] private Material _CreateChunksFromScreenSpaceTexture;
+        [SerializeField] private Material _GenerateAsciiTexture;
+
+        [Header("Necassary Objects")]
+        [SerializeField] private SymbolList _SymbolList;
+        [SerializeField] private SymbolDefinition[] _ChoosedSymbolDefinition;
+        [SerializeField] private Camera _MainCamera;
+
+        [Header("Variables")]
         [Range(0, 4096)]
         [SerializeField] private int _ChunkSizeX;
         [Range(0, 4096)]
         [SerializeField] private int _ChunkSizeY;
-        [SerializeField] private Material _CreateChunksFromScreenSpaceTexture;
 
-        [SerializeField] private Camera _MainCamera;
-
-        [SerializeField] private ComputeShader _GenerateAsciiForScreen;
-
+        [Header("Textures")]
         [SerializeField] private RenderTexture _ResultTexture;
 
-        [SerializeField] private SymbolDef[] _SymbolDefinitionBuffer;
+        [SerializeField] private RenderTexture _ResultHighQuality;
+
+        [SerializeField] private RenderTexture _AsciiTexture;
+
+        [SerializeField] private Texture2DArray _Texture2DArray;
         #endregion Inspector Variables
 
         #region Unity Methods
 
         private void Start()
         {
-            _CreateChunksFromScreenSpaceTexture = new Material(Shader.Find("Krakjam/ScreenSpaceChunks"));
+            var chunksCount = new Vector2(_MainCamera.pixelWidth / _ChunkSizeX, _MainCamera.pixelHeight / _ChunkSizeY);
 
-            _KernelId = _GenerateAsciiForScreen.FindKernel("ScreenSpaceAsciiGenerator");
-            _GenerateAsciiForScreen.SetVector(_ScreenSpaceResolutionId, _Resolution);
+            _LessQualityResolution = new Vector2(chunksCount.x * 2, chunksCount.y * 2);
+
+            _CreateChunksFromScreenSpaceTexture = new Material(Shader.Find("Krakjam/ScreenSpaceChunks"));
+            _GenerateAsciiTexture = new Material(Shader.Find("Krakjam/AsciiTexture"));
 
             _SplitScreenTexture = new RenderTexture(_MainCamera.pixelWidth, _MainCamera.pixelHeight, 24, RenderTextureFormat.ARGBFloat)
             {
@@ -39,11 +54,14 @@ namespace Krakjam
 
             _SplitScreenTexture.Create();
 
-            _ChunksCount = new Vector2(_MainCamera.pixelWidth / _ChunkSizeX, _MainCamera.pixelHeight / _ChunkSizeY);
+            _ResultHighQuality = new RenderTexture(_MainCamera.pixelWidth, _MainCamera.pixelHeight, 24, RenderTextureFormat.ARGBFloat)
+            {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp,
+                enableRandomWrite = true
+            };
 
-            _LessQualityResolution = new Vector2(_ChunksCount.x * 2, _ChunksCount.y * 2);
-
-            _SymbolDefinitionBuffer = new SymbolDef[(int)_ChunksCount.x * (int)_ChunksCount.y];
+            _ResultHighQuality.Create();
 
             _SplitScreenTextureLessQuality = new RenderTexture((int)_LessQualityResolution.x, (int)_LessQualityResolution.y, 24, RenderTextureFormat.ARGBFloat)
             {
@@ -63,101 +81,135 @@ namespace Krakjam
 
             _ResultTexture.Create();
 
-            _Resolution = new Vector2(_LessQualityResolution.x, _LessQualityResolution.y);
+            _AsciiTexture = new RenderTexture(_MainCamera.pixelWidth, _MainCamera.pixelHeight, 24, RenderTextureFormat.ARGBFloat)
+            {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp,
+                enableRandomWrite = true
+            };
+
+            _AsciiTexture.Create();
+
+            CreateTextureArray();
+
+            _CurrentSymbolsDefinition = new SymbolDefs[_SymbolList.Definitions.Count];
+
+            FillSymbolsDefinition();
+
+            _SymbolDefinitionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _SymbolList.Definitions.Count, 5 * sizeof(float));
+
+            _KernelId = _PrepareTextureToGenerateAsciiTexture.FindKernel("PrepareToCreateAsciiTexture");
         }
 
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
-            Graphics.Blit(source, destination);
             _CreateChunksFromScreenSpaceTexture.SetInt(_ChunkSizeXId, _ChunkSizeX);
             _CreateChunksFromScreenSpaceTexture.SetInt(_ChunkSizeYId, _ChunkSizeY);
             Graphics.Blit(source, _SplitScreenTexture, _CreateChunksFromScreenSpaceTexture);
             Graphics.Blit(_SplitScreenTexture, _SplitScreenTextureLessQuality);
 
-            _GenerateAsciiForScreen.SetTexture(_KernelId, _ChunkScreenTextureId, _SplitScreenTextureLessQuality);
-            _GenerateAsciiForScreen.SetTexture(_KernelId, _ResultTextureId, _ResultTexture);
+            _PrepareTextureToGenerateAsciiTexture.SetTexture(_KernelId, _ResultTextureId, _ResultTexture);
+            _PrepareTextureToGenerateAsciiTexture.SetTexture(_KernelId, _ScreenSpaceChunkTextureId, _SplitScreenTextureLessQuality);
 
-            _GenerateAsciiForScreen.SetInt(_ChunkSizeXId, _MainCamera.pixelWidth / _ChunkSizeX);
+            _SymbolDefinitionBuffer.SetData(_CurrentSymbolsDefinition);
 
-            _GenerateAsciiForScreen.Dispatch(_KernelId, (int)_LessQualityResolution.x, (int)_LessQualityResolution.y, 1);
+            _PrepareTextureToGenerateAsciiTexture.SetBuffer(_KernelId, _SimpleDefinitionId, _SymbolDefinitionBuffer);
 
-            Texture2D result = toTexture2D(_ResultTexture, new Vector2((int)_LessQualityResolution.x, (int)_LessQualityResolution.y));
-            var pixels = result.GetPixels();
-            int id = 0;
+            _PrepareTextureToGenerateAsciiTexture.SetInt(_TextureSizeId, _SymbolList.Definitions.Count);
 
-            for (int y = 0; y < result.height; y += 2)
-            {
-                for (int x = 0; x < result.width; x += 2)
-                {
-                    var index = x + y * result.width;
-                    var pixel = pixels[index];
-                    var left = (pixel.r + pixel.a) * 0.5f;
-                    var right = (pixel.g + pixel.a) * 0.5f;
-                    var top = (pixel.b + pixel.a) * 0.5f;
-                    var bottom = (pixel.r + pixel.g) * 0.5f;
-                    var avarage = (pixel.r + pixel.g + pixel.b + pixel.a) * 0.25f;
+            _PrepareTextureToGenerateAsciiTexture.Dispatch(_KernelId, _ResultTexture.width, _ResultTexture.height, 1);
 
-                    _SymbolDefinitionBuffer[id].left = left;
-                    _SymbolDefinitionBuffer[id].right = right;
-                    _SymbolDefinitionBuffer[id].top = top;
-                    _SymbolDefinitionBuffer[id].bottom = bottom;
-                    _SymbolDefinitionBuffer[id].avarage = avarage;
-                    ++id;
-                }
-            }
+            Graphics.Blit(_ResultTexture, _ResultHighQuality);
+
+            _GenerateAsciiTexture.SetTexture(_CharacterTextureArrayId, _Texture2DArray);
+
+            Graphics.Blit(_ResultHighQuality, _AsciiTexture, _GenerateAsciiTexture);
+
+            Graphics.Blit(_AsciiTexture, destination);
         }
         #endregion Unity Methods
 
         #region Private Types
-        [Serializable]
-        private struct SymbolDef
+        private struct SymbolDefs
         {
             public float left;
             public float right;
             public float top;
             public float bottom;
             public float avarage;
-
-            public SymbolDef(float lef, float rig, float tp, float bot, float avg)
+            public SymbolDefs(float l, float r, float b, float t, float a)
             {
-                left = lef;
-                right = rig;
-                top = tp;
-                bottom = bot;
-                avarage = avg;
+                left = l;
+                right = r;
+                bottom = b;
+                top = t;
+                avarage = a;
             }
-        }
+        };
         #endregion Private Types
 
         #region Private Variables
         private RenderTexture _SplitScreenTexture;
         private RenderTexture _SplitScreenTextureLessQuality;
-        private Vector2 _Resolution;
-
-        private int _KernelId;
-
-        private Vector2 _ChunksCount;
 
         private Vector2 _LessQualityResolution;
 
         private static readonly int _ChunkSizeXId = Shader.PropertyToID("_ChunkSizeX");
         private static readonly int _ChunkSizeYId = Shader.PropertyToID("_ChunkSizeY");
 
-        private static readonly int _ChunkScreenTextureId = Shader.PropertyToID("_ScreenSpaceTexutre");
-        private static readonly int _ScreenSpaceResolutionId = Shader.PropertyToID("_ScreenResolution");
+        private static readonly int _ScreenSpaceChunkTextureId = Shader.PropertyToID("_ScreenSpaceTexutre");
         private static readonly int _ResultTextureId = Shader.PropertyToID("_ResultTexture");
+        private static readonly int _SimpleDefinitionId = Shader.PropertyToID("_SimpleDefinition");
+        private static readonly int _TextureSizeId = Shader.PropertyToID("_TextureSize");
+
+        private static readonly int _CharacterTextureArrayId = Shader.PropertyToID("_CharacterTextureArray");
         #endregion Private Variables
 
-        #region Private Variables
-        private Texture2D toTexture2D(RenderTexture rTex, Vector2 resolution)
+        #region Private Methods
+
+        private SymbolDefs[] _CurrentSymbolsDefinition;
+        private GraphicsBuffer _SymbolDefinitionBuffer;
+
+        private int _KernelId;
+
+        private void CreateTextureArray()
         {
-            Texture2D tex = new Texture2D((int)resolution.x, (int)resolution.y, TextureFormat.RGB24, false);
-            // ReadPixels looks at the active RenderTexture.
-            RenderTexture.active = rTex;
-            tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
-            tex.Apply();
-            return tex;
+            // Create Texture2DArray
+            var characterTexture = _SymbolList.Definitions[0].Texture;
+
+            Texture2DArray texture2DArray = new
+                Texture2DArray(characterTexture.width,
+                characterTexture.height, _SymbolList.Definitions.Count,
+                TextureFormat.RGBA32, true, false);
+            // Apply settings
+            texture2DArray.filterMode = FilterMode.Bilinear;
+            texture2DArray.wrapMode = TextureWrapMode.Repeat;
+            // Loop through ordinary textures and copy pixels to the
+            // Texture2DArray
+            for (int i = 0; i < _SymbolList.Definitions.Count; i++)
+            {
+                var texture = _SymbolList.Definitions[i].Texture;
+                texture2DArray.SetPixels(texture.GetPixels(0),
+                    i, 0);
+            }
+            // Apply our changes
+            texture2DArray.Apply();
+
+            _Texture2DArray = texture2DArray;
         }
-        #endregion Private Variables
+
+        private void FillSymbolsDefinition()
+        {
+            for (int i = 0; i < _SymbolList.Definitions.Count; ++i)
+            {
+                _CurrentSymbolsDefinition[i].left = _SymbolList.Definitions[i].Left;
+                _CurrentSymbolsDefinition[i].right = _SymbolList.Definitions[i].Right;
+                _CurrentSymbolsDefinition[i].top = _SymbolList.Definitions[i].Top;
+                _CurrentSymbolsDefinition[i].bottom = _SymbolList.Definitions[i].Bottom;
+                _CurrentSymbolsDefinition[i].avarage = _SymbolList.Definitions[i].Average;
+            }
+        }
+
+        #endregion Private Methods
     }
 }
